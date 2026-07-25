bd8ddd135121ab4441f6da89713e738c698de494
rachittomar9258 <rachittomar67@gmail.com>
2026-07-25 15:00:38 +0530

    Add temporary debug endpoint

--- tubewell_system/urls.py
+++ tubewell_system/urls.py
@@ -5,12 +5,18 @@ from django.http import JsonResponse
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
+from django.conf import settings
 
 def debug_users(request):
     User = get_user_model()
     users = list(User.objects.values('username', 'role', 'is_staff', 'is_superuser', 'is_active'))
     return JsonResponse({'users': users, 'count': len(users)})
 
+from django.conf import settings
+
+def debug_db(request):
+    db = settings.DATABASES['default']
+    return JsonResponse({'engine': db.get('ENGINE'), 'host': db.get('HOST'), 'name': db.get('NAME')})
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
@@ -20,4 +26,5 @@ urlpatterns = [
     path('usage/', include('usage.urls')),
     path('payments/', include('payments.urls')),
     path('debug-users-temp-xyz/', debug_users),
+    path('debug-db-temp-xyz/', debug_db),
   ]
